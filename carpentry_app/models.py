d8736033a070ccb0f351ad8f6bc50aa70a9ffe5d
SaurabhVishwakarma826 <vishwakarmasaurabh826@gmail.com>
2024-01-01 12:27:16 +0530

    images added

--- carpentry_app/models.py
+++ carpentry_app/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class ProductImage(models.Model):
     image = models.ImageField(upload_to='product_images/')
     likes = models.IntegerField(default=0)
-    description = models.TextField()
 
 class Testimonial(models.Model):
     name = models.TextField()
